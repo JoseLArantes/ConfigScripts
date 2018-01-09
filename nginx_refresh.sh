db3535092eab668d83379be1ec014bbdc1cb894f
Jose Arantes <jlarantes@gmail.com>
2018-01-09 18:09:10 -0200

    Now a real script.
    Need to improve no changes in test failure.

--- nginx_refresh.sh
+++ nginx_refresh.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sudo service php5.6-fpm restart
-sudo service nginx stop
-sleep 10
-sudo service nginx start
+old=41
+count=$(find /etc/nginx/sites-enabled -maxdepth 1 -type l|wc -l)
+
+if
+        [ $count \> $old ];
+then
+	/etc/init.d/nginx configtest && sudo /etc/init.d/nginx reload;
+        sed -i -e "s/$old/$count/g" /etc/nginx/sites-available/zcheck_nginx_changes_on_infrastructure.sh;
+	touch /var/log/nginx/zcheck.log;
+	echo "Change noticed. Nginx reboot at $(date "+%F %T %Z")" >> /var/log/nginx/zcheck.log;
+else
+        echo "No";
+fi
