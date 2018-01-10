1155548d1e6459ea895884eded95563e9784636e
Jose Arantes <jlarantes@gmail.com>
2018-01-10 00:48:42 -0200

    Restart nginx insted of reload

--- nginx_refresh.sh
+++ nginx_refresh.sh
@@ -5,7 +5,7 @@ count=$(find /etc/nginx/sites-enabled -maxdepth 1 -type l|wc -l)
 if
         [ $count \> $old ];
 then
-	/etc/init.d/nginx configtest && sudo /etc/init.d/nginx reload;
+	/etc/init.d/nginx configtest && sudo /etc/init.d/nginx restart;
         sed -i -e "s/$old/$count/g" /run/nginx_refresh.sh;
 	touch /var/log/nginx/zcheck.log;
 	echo "Change noticed. Nginx reboot at $(date "+%F %T %Z")" >> /var/log/nginx/zcheck.log;
